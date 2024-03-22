79278515bdcdda7d67246951a3eadef30bfdb75c
Tejas Parikh <tejas12parikh12@gmail.com>
2024-03-22 19:28:15 +0530

    db url

--- connection/database.py
+++ connection/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_database = 'postgresql://postgres:83527@localhost:5432/metaworkBackend'
+URL_database = 'postgresql://postgres:tejas83527@metaworkbackend.clyqy0y02fiu.us-west-1.rds.amazonaws.com:5432/metaworkBackend'
 
 engine = create_engine(URL_database)
 
